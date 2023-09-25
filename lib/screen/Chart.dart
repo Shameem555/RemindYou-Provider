02c0302c5e3fa390bf1e11b41e638cf8bec169e7
Shameem555 <shameemta555@gmail.com>
2023-09-25 19:45:55 +0530

    chart UI is created

--- lib/screen/Chart.dart
+++ lib/screen/Chart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Chart extends StatefulWidget {
   const Chart({super.key});
@@ -8,6 +9,17 @@ class Chart extends StatefulWidget {
 }
 
 class _ChartState extends State<Chart> {
+
+  late List <ChartData> _chartDatas;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    _chartDatas = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -15,18 +27,46 @@ class _ChartState extends State<Chart> {
         backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: Colors.blue[300],
-          shape: RoundedRectangleBorder(
+          shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.vertical(
               bottom: Radius.circular(20),
             ),
           ),
-          title: Text("Chart",style: TextStyle(
+          title: const Text("Chart",style: TextStyle(
               fontSize: 25,
-              fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.bold,  
             ),),
           centerTitle: true,
         ),
+        body: SfCircularChart(
+          legend: const Legend(isVisible: true, overflowMode: LegendItemOverflowMode.wrap),
+          tooltipBehavior: _tooltipBehavior,
+          series: <CircularSeries>[
+          PieSeries <ChartData, String> ( 
+            dataSource: _chartDatas,
+            xValueMapper: (ChartData data, _)=> data.continent,
+            yValueMapper:(ChartData data, _) => data.gd,
+            dataLabelSettings: const DataLabelSettings(isVisible: true),
+            enableTooltip: true,
+          )
+        ],),
       ),
-    );
+    ); 
   }
+  List <ChartData> getChartData(){
+      final List < ChartData > chartDatas =[
+        ChartData('Birthday', 3500),
+        ChartData('Wedding', 1000),
+        ChartData('House Warming', 2000),
+        ChartData('Engagement', 2000),
+        ChartData('Inaguration', 1000),
+      ];
+      return chartDatas;
+    }
+}
+
+class ChartData {
+  ChartData(this.continent,this.gd);
+  final String continent;
+  final int gd;
 }
