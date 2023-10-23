35ac8822793f01372d4e302b5a66fe244c0c1039
Shameem555 <shameemta555@gmail.com>
2023-10-23 10:37:01 +0530

    home screen small change

--- lib/screen/homedisplayscreen.dart
+++ lib/screen/homedisplayscreen.dart
@@ -141,6 +141,7 @@ class DisplayScreen extends StatefulWidget {
   const DisplayScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _DisplayScreenState createState() => _DisplayScreenState();
 }
 
