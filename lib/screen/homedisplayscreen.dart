5db98af99fbf67fdb7e2f99de8bff4363764b520
Shameem555 <shameemta555@gmail.com>
2023-10-27 17:27:08 +0530

    command remove

--- lib/screen/homedisplayscreen.dart
+++ lib/screen/homedisplayscreen.dart
@@ -62,12 +62,6 @@
 //             subtitle: Text(
 //                 '${data.dateTime.day}/${data.dateTime.month}/${data.dateTime.year}'),
 //             trailing: PopupMenuButton(
-//               itemBuilder: (context) => [
-//                 PopupMenuItem(
-//                   child: Row(
-//                     children: [
-//                       Padding(
-//                         padding: const EdgeInsets.only(left: 10.0),
 import 'package:flutter/material.dart';
 import 'package:reminder/main.dart';
 import 'package:reminder/model/data_model.dart';
