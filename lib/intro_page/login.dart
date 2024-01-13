ab16af1b0eb9c0b3da39023ffffa9781aec8396b
Shameem Ali <shameemta555@gmail.com>
2024-01-13 11:57:43 +0530

    login page changed to provider package

--- lib/intro_page/login.dart
+++ lib/intro_page/login.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reminder/controller/login-provider.dart';
 import 'package:reminder/intro_page/bottomBar.dart';
 import 'package:reminder/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final _usernameController = TextEditingController();
+// final _usernameController = TextEditingController();
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -12,7 +14,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final _formKey = GlobalKey<FormState>();
+  // final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -26,77 +28,82 @@ class _LoginState extends State<Login> {
         // ignore: sized_box_for_whitespace
         child: Container(
           height: screenHeight,
-          child: Column(
-            children: [
-              const SizedBox(height: 100),
-              Center(
-                  child: SizedBox(
-                height: 300,
-                width: 300,
-                child: Image.asset(
-                  'assets/empty_profile.jpeg',
-                  fit: BoxFit.fill,
-                ),
-              ),
-              ),
-              const SizedBox(height: 30,),
-              Form(
-                key: _formKey,
-                child: SizedBox(
-                  width: screenWidth * .8,
-                  height: screenHeight * .08,
-                  child: TextFormField(
-                    controller: _usernameController,
-                    decoration: InputDecoration(
-                        isDense: true,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        hintText: 'Username',hintStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 25)
-                        ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Required Name';
-                      } else {
-                        return null;
-                      }
-                    },
+          child: Consumer<LoginProvider>(
+            builder: (context, provider, child) {
+              return Column(
+              children: [
+                const SizedBox(height: 100),
+                Center(
+                    child: SizedBox(
+                  height: 300,
+                  width: 300,
+                  child: Image.asset(
+                    'assets/empty_profile.jpeg',
+                    fit: BoxFit.fill,
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: GestureDetector(
-                  onTap: () {
-                    if (_formKey.currentState!.validate()) {
-                      checkLogin(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const BottomBar()),
-                      );
-                    }
-                  },
-                  child: Container(
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color:  Colors.blue,
+                ),
+                const SizedBox(height: 30,),
+                Form(
+                  key: provider.formKey,
+                  child: SizedBox(
+                    width: screenWidth * .8,
+                    height: screenHeight * .08,
+                    child: TextFormField(
+                      controller: provider.usernameController,
+                      decoration: InputDecoration(
+                          isDense: true,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          hintText: 'Username',hintStyle: const TextStyle(fontWeight: FontWeight.bold,fontSize: 25)
+                          ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Required Name';
+                        } else {
+                          return null;
+                        }
+                      },
                     ),
-                    width: size.width * 0.3,
-                    height: size.height * 0.06,
-                    child: const Text(
-                      'Save',
-                      style: TextStyle(
-                        fontFamily: 'f',
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white,
-                        fontSize: 17,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: GestureDetector(
+                    onTap: () {
+                      if (provider.formKey.currentState!.validate()) {
+                        checkLogin(context);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const BottomBar()),
+                        );
+                      }
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color:  Colors.blue,
+                      ),
+                      width: size.width * 0.3,
+                      height: size.height * 0.06,
+                      child: const Text(
+                        'Save',
+                        style: TextStyle(
+                          fontFamily: 'f',
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            );
+            },
+             
           ),
         ),
       ),
@@ -104,7 +111,8 @@ class _LoginState extends State<Login> {
   }
 
   void checkLogin(BuildContext ctx) async {
-    final username = _usernameController.text;  
+    final provider = Provider.of<LoginProvider>(context,listen: false);
+    final username = provider.usernameController.text;  
 
     final sharedPref = await SharedPreferences.getInstance();
     await sharedPref.setBool(saveKeyName, true);
